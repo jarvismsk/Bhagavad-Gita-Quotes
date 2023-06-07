adada24d228f6e9e29eaaf3fe06f980d67e61d5a
Irfan Khan <jarvismsk@gmail.com>
2023-06-07 23:14:19 +0530

    added arrays

---
+++ Javascript Projects/Bhagavad Gita Quotes/main.js
@@ -0,0 +1,8 @@
+const gitaquotes = [
+  "One who sees inaction in action, and action in inaction, is intelligent among men.",
+  "You should perform your duty with a view to guide people and for universal welfare.",
+  "Even the wise are confused about what is action and what is inaction."
+  ];
+
+  document.getElementById("dquote").innerHTML = gitaquotes[2];
+
